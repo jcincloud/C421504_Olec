fccb904df8362ecbacb65db015518f27bc365910
TinWu <yyuuix@gmail.com>
2015-08-31 10:26:00 +0800

    漏掉的檔案

---
+++ Work.WebProj/Content/js/scroll.js
@@ -0,0 +1,21 @@
+﻿//test
+$(window).scroll(function(){
+     if ($(this).scrollTop() > 100) {
+          $('.goTop').fadeIn();
+     } else {
+          $('.goTop').fadeOut();
+     }
+});
+$(document).ready(function(){
+     $('.goTop').click(function(){
+          $("html, body").animate({ scrollTop: 0 }, 600);
+          return false;
+     });
+
+     $('.scroll').click(function () {
+         $('html, body').animate({
+             scrollTop: $($.attr(this, 'href')).offset().top
+         }, 600);
+         return false;
+     });
+});
