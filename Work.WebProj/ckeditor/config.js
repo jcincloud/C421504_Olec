60daf550baa8f75248c1730b7a8fb5361d232502
TinWu <yyuuix@gmail.com>
2015-09-10 10:57:58 +0800

    編輯器toolbar修改

--- Work.WebProj/ckeditor/config.js
+++ Work.WebProj/ckeditor/config.js
@@ -12,7 +12,7 @@ CKEDITOR.editorConfig = function (config) {
     config.toolbar = [
         {
             name: "basicstyles",
-            items: ["FontSize", "Bold", "Italic", "-", "JustifyLeft", "JustifyCenter", "JustifyRight", 'Font']
+            items: ["FontSize", "Bold", "Italic", "-", "JustifyLeft", "JustifyCenter", "JustifyRight"]
         },
         {
             name: "paragraph",
@@ -21,20 +21,14 @@ CKEDITOR.editorConfig = function (config) {
             name: "tools",
             items: ["Maximize", "-"]
         }, {
-            name: "styles",
-            items: ["Styles"]
-        }, {
             name: "links",
-            items: ["Link", "Unlink", "Anchor"]
+            items: ["Link", "Unlink"]
         }, {
             name: 'insert',
-            items: ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe']
+            items: ['Image', 'Table', 'Smiley', 'Iframe']
         }, {
             name: "colors",
             items: ["TextColor", "BGColor"]
-        }, { name: "editing" }, {
-            name: "document",
-            items: ["Source", "-", "DocProps"]
         }, {
             name: "clipboard",
             items: ["Cut", "Copy", "Paste", "PasteText", "PasteFromWord", "Undo", "Redo"]
@@ -43,4 +37,5 @@ CKEDITOR.editorConfig = function (config) {
     config.filebrowserBrowseUrl = "../ckfinder/ckfinder.html";
     config.filebrowserImageBrowseUrl = "../ckfinder/ckfinder.html?type=Images";
     config.filebrowserImageUploadUrl = "../ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images";
+    config.autoUpdateElement = true;
 };
